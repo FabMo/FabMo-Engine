7223fa38349f2a9d0f3cc0dc8b43ff60d5b7f0c3
Ryan Sturmer <ryansturmer@gmail.com>
2014-03-28 07:29:19 -0400

    First working shopbot example with mock data

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
+app.debug = True
+
+import random
+TOOLS = []
+TOOL_NAMES = ['Shopbot Desktop', 'Shopbot Buddy', 'Shopbot PRS', 'Shopbot Alpha']
+STATUSES = ['idle','running','homing']
+for i in range(10):
+    tool = {'name': random.choice(TOOL_NAMES),
+            'xpos': random.random()*24.0,
+            'ypos': random.random()*18.0,
+            'zpos': random.random()*4.0,
+            'status': random.choice(STATUSES),
+            'id': i}
+    TOOLS.append(tool)
 
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
+
+@app.route('/tools')
+def tools():
+    return jsonify({'tools':TOOLS})
 
 if __name__ == '__main__':
     app.run()
--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,29 @@
 App = Ember.Application.create();
+//App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 App.Router.map(function() {
-  // put your routes here
+    this.resource('tools');
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+App.Tool = DS.Model.extend({
+    name : DS.attr('string'),
+    xpos : DS.attr('number'),
+    ypos : DS.attr('number'),
+    zpos : DS.attr('number'),
+    status : DS.attr('string'),
+});
+
+App.Tool.FIXTURES = [{
+    id: 1,
+    name: 'Shopbot Desktop',
+    xpos: 10.3,
+    ypos: 9.1,
+    zpos: 1.225,
+    status: 'idle'
+}];
+
+App.ToolsRoute = Ember.Route.extend({
+    model: function() {
+        return this.store.find('tool')
+    }
 });
