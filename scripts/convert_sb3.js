f7de4bf9eadfc345cf2ca1def43106e7ebbbae58
Ryan Sturmer <ryansturmer@gmail.com>
2014-07-29 08:15:35 -0400

    Improve parsing of prm/cmd files

--- scripts/convert_sb3.js
+++ scripts/convert_sb3.js
@@ -10,6 +10,12 @@ function scrubName(name) {
 	return name
 }
 
+function parseDefault(dflt) {
+	parts = dflt.split(/[-=](?![0-9])/);
+	console.log(parts);
+	return new Array(parts[0].trim(), parts[1].trim())
+}
+
 // Deal with commands 
 var commands = {}
 
@@ -37,48 +43,61 @@ csv().from.string(cmd).on('record', function(record, idx) {
 	command.name = scrubName(fullname)
 
 	if(command.name != "<seperator>" && cmd != 'H5') {
-		commands.cmd = command;
-		console.log(command);
+		commands[cmd] = command;
 	}
-});
-
-
-row_count = 0;
-var param_list;
-var prm_done = false;
-var params = {};
-var current_cmd = null;
-
-csv().from.string(prm).on('record', function(record, idx) {
-	if(row_count < 2) {
-		row_count += 1;
-		return;
-	}
-	row_count += 1;
-
-	if(record[0] != '') {
-		if(current_cmd != null) {
-			params[current_cmd] = param_list;
-		}
-		current_cmd = record[1];
-		nparams = parseInt(record[0]);
-		console.log(param_list)
-		param_list = [];
-	}
-	param = {}
-	param.name = record[4];
-	param.desc = record[5];
-	param.abrev = record[6];
-	param.type = record[7];
-	param.typeext = parseInt(record[8]);
-	param.default = record[10];
-	console.log(param);
-	param_list.push(param);
 }).on('end', function(count) {
-	fs.writeFile("prm.json",JSON.stringify(params, null, 3));
+	console.log(commands);
+	var	row_count = 0;
+	var param_list = [];
+	var prm_done = false;
+	var params = {};
+	var current_cmd = null;
+	var current_param = {};
+
+	csv().from.string(prm).on('record', function(record, idx) {
+		if(row_count < 2) {
+			row_count += 1;
+			return;
+		}
+		row_count += 1;
+
+		if(record[0] != '') {
+			param_list.push(current_param);
+			current_param = {}
+			if(current_cmd != null) {
+				console.log(current_cmd);
+				commands[current_cmd].params = param_list;
+			}
+			current_cmd = record[1];
+			nparams = parseInt(record[0]);
+			param_list = [];
+		}
+		current_param.type = record[7];
+		switch(current_param.type) {
+			case 'ops':
+			case 'opt':
+				if(record[6] == '') {
+					current_param.name = record[4];
+					current_param.desc = record[5];
+					current_param.abrev = record[6];
+					current_param.typeext = parseInt(record[8]);
+					current_param.default = parseDefault(record[10]);
+				} else {
+					current_param.opts = [];
+				}
+				current_param.opts.push(parseDefault(record[10]));
+			break;
+			default:
+				current_param.name = record[4];
+				current_param.desc = record[5];
+				current_param.abrev = record[6];
+				current_param.typeext = parseInt(record[8]);
+				current_param.default = record[10];
+			break;		
+		}
+	}).on('end', function(count) {
+		fs.writeFile("cmd.json",JSON.stringify(commands, null, 3));
+	});
 });
- 
 
-console.log('for real.')
 
-//fs.writeFile("cmd.json",JSON.stringify(commands));
