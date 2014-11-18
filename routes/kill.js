3a7f5aa75b2f87e966373786a0309a31adaf5787
Ryan Sturmer <ryansturmer@gmail.com>
2014-11-17 23:21:08 -0500

    Add warning

--- routes/kill.js
+++ routes/kill.js
@@ -1,6 +1,8 @@
 process = require('process');
+log = require('../log').logger('routes')
 
 kill = function(req, res, next) {
+    log.warn('Killing the engine by user request...');
     res.json({'success':true});
     process.exit(1);
 };
