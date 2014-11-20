433c96cda4536d2172835827f44bcf0c03f0412e
Ryan Sturmer <ryansturmer@gmail.com>
2014-11-20 14:17:40 -0700

    Add simple run script

---
+++ temp/run_g2.js
@@ -0,0 +1,17 @@
+var process = require('process');
+var serialport = require("serialport");
+var fs = require('fs');
+
+filename = process.argv[2]
+
+data = fs.readFileSync(filename) + '\n';
+console.log(data);
+port = new serialport.SerialPort('/dev/ttyACM0', false);	
+
+port.open( function(error) {
+	if(error) {
+		console.log(error);
+	} else {
+		port.write(data);
+	}
+});
