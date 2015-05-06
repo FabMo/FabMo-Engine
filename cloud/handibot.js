c4374b6b86f8b3ddc31278db49254604f690310d
Ryan Sturmer <ryansturmer@gmail.com>
2015-05-05 22:56:06 -0400

    Some handibot.net stuff

---
+++ cloud/handibot.js
@@ -0,0 +1,39 @@
+var request = require('request');
+
+var HandibotProvider = function(options) {
+	this.username = options.username;
+	this.password = options.password;
+}
+
+HandibotProvider.prototype.start = function() {
+
+}
+
+HandibotProvider.prototype.stop = function() {
+
+}
+
+HandibotProvider.prototype.update = function() {
+
+}
+
+HandibotProvider.prototype._login = function(callback) {
+	var data = {
+		'email' : this.username,
+		'password' : this.password,
+		'who' : 'FabMo'
+	}
+	request.post(
+		'http://handibot.com/ajax_proc/login.php', 
+		{	'content-type' : 'application/json', 
+			body:JSON.stringify(data)
+		}, 
+		function(err, res, body) {
+			console.log(body);
+			//console.log(res.toJSON())
+		}
+	);
+}
+
+var hb = new HandibotProvider({'username':'ryansturmer', 'password':'shop4bot'});
+hb._login();
