76716669809e16bc05d132c71ad961754f27f2d8
Ryan Sturmer <ryasnturmer@gmail.com>
2015-07-02 12:38:18 -0400

    Add debug mode for regular apps too

--- debug.js
+++ debug.js
@@ -1,38 +1,48 @@
 var dashboard = require('./dashboard');
 var log = require('./log').logger('debug');
+var config = require('./config');
+
 var watch_semaphore = 0;
 var NCP_TIMEOUT = 4000;
+
+var appReloader = function(event, path, details) {
+  // Don't watch for changes if there is an update in progress
+  if(watch_semaphore) { return; }
+
+  // Determine which app changed, and re-copy that app
+  app_index = dashboard.getAppIndex();
+  for(app_id in app_index) {
+    app_info = app_index[app_id];
+    if(path.indexOf(app_info.app_archive_path) >= 0) {
+      log.info(app_id + ' was changed. Reloading...');
+      watch_semaphore+=1;
+      var timeout = setTimeout(function() {
+        log.warn('Timeout waiting for reload of ' + app_id);
+        watch_semaphore-=1;
+      }, NCP_TIMEOUT);
+      return dashboard.reloadApp(app_id, function(err, result) {
+        clearTimeout(timeout);
+        log.info(app_id + ' updated.');
+        watch_semaphore-=1;  
+      });        
+    }
+  }
+};
+
 function startDebug() {
   var chokidar = require('chokidar');
   var watcher = chokidar.watch('./dashboard/apps', {
     ignored: /[\/\\]\./,
     persistent: true
   });
+  watcher.on('raw', appReloader);
 
-  watcher
-    .on('raw', function(event, path, details) {
-      // Don't watch for changes if there is an update in progress
-      if(watch_semaphore) { return; }
+  var watcher = chokidar.watch(config.getDataDir('apps'), {
+    ignored: /[\/\\]\./,
+    persistent: true
+  });
+  watcher.on('raw', appReloader);
 
-      // Determine which app changed, and re-copy that app
-      app_index = dashboard.getAppIndex();
-      for(app_id in app_index) {
-        app_info = app_index[app_id];
-        if(path.indexOf(app_info.app_archive_path) >= 0) {
-          log.info(app_id + ' was changed. Reloading...');
-          watch_semaphore+=1;
-          var timeout = setTimeout(function() {
-            log.warn('Timeout waiting for reload of ' + app_id);
-            watch_semaphore-=1;
-          }, NCP_TIMEOUT);
-          return dashboard.reloadApp(app_id, function(err, result) {
-            clearTimeout(timeout);
-            log.info(app_id + ' updated.');
-            watch_semaphore-=1;  
-          });        
-        }
-      }
-    });
 }
 
 exports.start = startDebug;
