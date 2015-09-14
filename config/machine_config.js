092d42501b6b3b346486a156fe6e34d0a3cbc2a1
Ryan Sturmer <ryasnturmer@gmail.com>
2015-09-14 12:31:01 -0400

    Fix machine config not to crash in the absence of G2

--- config/machine_config.js
+++ config/machine_config.js
@@ -30,18 +30,22 @@ MachineConfig.prototype.update = function(data, callback) {
 
 MachineConfig.prototype.apply = function(callback) {
 	try {
-		// 0 = g21 = 
-		var units = this.get('units');
-		if(units === 'in' || units === 0) {
-			log.info("Changing default units to INCH");
-			config.driver.changeUnits(0, callback);
-		} else if(units === 'mm' || units === 1) {
-			log.info("Changing default units to MM");
-			config.driver.changeUnits(1, callback);
+		if(config.driver.changeUnits) {
+			var units = this.get('units');
+			if(units === 'in' || units === 0) {
+				log.info("Changing default units to INCH");
+				config.driver.changeUnits(0, callback);
+			} else if(units === 'mm' || units === 1) {
+				log.info("Changing default units to MM");
+				config.driver.changeUnits(1, callback);
+			} else {
+				log.warn('Invalid units "' + gc + '"found in machine configuration.');
+				callback(null);
+			}
 		} else {
-			log.warn('Invalid units "' + gc + '"found in machine configuration.');
 			callback(null);
 		}
+
 	}
 	catch (e) {
 		callback(e);
