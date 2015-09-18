b7ed3f8113da9848e3a4b34312ae74d4b794257b
Brendan Collins <brendan.c.collins@gmail.com>
2015-09-18 17:00:14 -0400

    Main menu large in desktop and collapsed in mobile view (still not perfect)

--- dashboard/static/js/events.js
+++ dashboard/static/js/events.js
@@ -5,12 +5,21 @@ resizedoc = function(){
 	//L & R = width of left & right menus
 	var l=0; var r=0;
 
-	//If medium screen
+	//Screen displays based on size
+	
 	if($("body").width()/parseFloat($("body").css("font-size"))>40.063) {
 		$("#main").addClass("offcanvas-overlap-right"); //Show Left Menu
+		$("#widget-links-general").removeClass("colapsed");//Show fulll left hand menu on full screen
+		$("#left-menu").removeClass("colapsed");//Show fulll left hand menu on full screen
 		l=parseInt($("#left-menu").css("width"))+1; //Save left menu size
 	}
-	else {
+	else if ($("body").width()/parseFloat($("body").css("font-size"))<=40.063) {
+		$("#main").addClass("offcanvas-overlap-right"); //Show Left Menu
+		$("#widget-links-general").addClass("colapsed");//Show fulll left hand menu on full screen
+		$("#left-menu").addClass("colapsed");//Show fulll left hand menu on full screen
+		l=parseInt($("#left-menu").css("width"))+1; //Save left menu size
+		$('#close_menu').css('display', 'none');
+	} else {
 		//No left & right menus
 		$("#widget-links-general").removeClass("colapsed");
 		$("#left-menu").removeClass("colapsed");
@@ -18,6 +27,7 @@ resizedoc = function(){
 		$("#main").removeClass("offcanvas-overlap-left");
 		l=0;
 	}
+
 
 	//If wide screen and Right menu
 	if( ($("#main").hasClass("offcanvas-overlap-left")) && ($("body").width()/parseFloat($("body").css("font-size")))>60.063) {
@@ -42,20 +52,45 @@ resizedoc = function(){
 
 
 colapseMenu = function() {
+	//L & R = width of left & right menus
+	var l=0; var r=0;
 	//If menu is colapsed, we remove this property
 	if($("#widget-links-general").hasClass("colapsed"))	{
 		$("#widget-links-general").removeClass("colapsed");
 		$("#left-menu").removeClass("colapsed");
+		l=parseInt($("#left-menu").css("width"))+1; //Save left menu size
+		if ($("body").width()/parseFloat($("body").css("font-size"))) {
+			$('#close_menu').css('display', 'block'); // show tinted screen to close menu
+		}
 	}
 
 	//If menu not colapsed, we colapse it
 	else {
 		$("#widget-links-general").addClass("colapsed");
 		$("#left-menu").addClass("colapsed");
+		$("#app_menu_container").css("width",
+			$(".main-section").width() - ($(".main-section").width() % 132 )
+		);
+		$('#close_menu').css('display', 'none');	
+		l=parseInt($("#left-menu").css("width"))+1; //Save left menu size		
 	}
 
 	//As the size of document change, we call this function to ajust main div & app container size
-	resizedoc();
+	//resizedoc();
+		//If wide screen and Right menu
+	if ($("body").width()/parseFloat($("body").css("font-size"))>40.063) {
+		if( ($("#main").hasClass("offcanvas-overlap-left")) && ($("body").width()/parseFloat($("body").css("font-size")))>60.063) {
+			r=parseInt($("#right-menu").css("width")+1);
+		} else {r=0;}
+	
+		//Save & calcul right menu size + 1px of margin
+		r=r+l;
+		
+		//Set size attribute on div "Main Section"
+		$(".main-section, .app-section").css("width",$("body").width()-r+"px");
+		$(".main-section, .app-section").css("margin-left",l+"px");
+		$(".main-section").css("height",$("#left-menu").height()+"px");
+	}
 };
 
 resizedocclick = function(){
@@ -103,13 +138,17 @@ $(document).ready( function() {
     });	
 
 	//Init of the size of the main & app-icon containers
+$(document).ready(function() {
 	resizedoc();
-	
+});
 	//If size of the screen change, we resize the main & app container
 	$(window).resize( function() {resizedoc();});
 
 	//Idem if we colapse or un-colapse the right menu
-	$("#icon_colapse").click(function() { colapseMenu(); });
+	$("#icon_colapse").click(function() { 
+		colapseMenu(); 
+		
+		});
 
 	//Define the positions of the dashboard notifications
 	toastr.options["positionClass"] = "toast-bottom-center";
