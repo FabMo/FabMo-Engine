2a20e1077d9336390b512f1e78c8beeedf85c3e9
Ryan Sturmer <ryansturmer@gmail.com>
2015-10-14 08:22:33 -0400

    Delete old files

--- temp/TestSBP_temp.js
+++
@@ -1,8 +0,0 @@
-Engine = require('../engine').Engine;
-
-var engine = new Engine();
-
-engine.start(function(error, result){ 
-	engine.machine.sbp("JS,30,30,6,8,1");
-	setTimeout(process.exit,1000);
-});
