d9c94d68ae52bd0108fd4cbb73e26e15527b985d
Ryan Sturmer <ryansturmer@gmail.com>
2016-01-12 17:13:40 +0000

    Full support for network redirect

--- routes/network.js
+++ routes/network.js
@@ -3,14 +3,15 @@ var log = require('../log').logger('wifi');
 var config =  require('../config');
 var network = require('../network');
 
-function networkGet(req, res, next) {
+function networkRedirect(req, res, next) {
 	var host = req.headers.host.split(':')[0].trim('/');
 	var path = req.params[0];
 	res.redirect(301, 'http://' + host + ':' + 9877 + '/' + path, next);	
 }
 
 module.exports = function(server) {
-	server.get(/(network\/.*)/, networkGet);
-	//server.post(/network\/(.*)/,networkPost);
-	//server.del(/network\/(.*)/,networkDel);
+	server.get(/(network\/.*)/, networkRedirect);
+	server.post(/(network\/.*)/, networkRedirect);
+	server.del(/(network\/.*)/, networkRedirect);
+	server.put(/(network\/.*)/, networkRedirect);
 };
