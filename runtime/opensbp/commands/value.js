afd55afd74294e744f9bd494a5584af22efcf037
gordonab <gordonab@gmail.com>
2016-01-13 14:04:03 -0500

    Change the way VA handles the new base coordinates

--- runtime/opensbp/commands/value.js
+++ runtime/opensbp/commands/value.js
@@ -12,7 +12,7 @@ exports.VA = function(args, callback) {
 		var setVA_G2 = {};
 		var setVA_SBP = {};
 		var newLocation = 0;
-		var unitConv = 1.0;
+		var unitConv = 1;
 		var offset = 0;
 
 		if ( this.machine.driver.status.unit === "in" ) {  // inches
@@ -20,28 +20,28 @@ exports.VA = function(args, callback) {
 		}
 
 		if (args[6] !== undefined) { 	//X Base Coordinate
-		    this.emit_gcode("G28.3 X" + args[6]);
-		    MPO.x = args[6] / unitConv;
+		    this.emit_gcode("G28.3 X" + (args[6] / unitConv) );
+		    MPO.x = args[6];
 		}
 		if (args[7] !== undefined) { 	//Y Base Coordinate
-		    this.emit_gcode("G28.3 Y" + args[7]);
-		    MPO.y = args[7] / unitConv;
+		    this.emit_gcode("G28.3 Y" + (args[7] / unitConv) );
+		    MPO.y = args[7];
 		}
 		if (args[8] !== undefined) { 	//Z Base Coordinate
-		    this.emit_gcode("G28.3 Z" + args[8]);
-		    MPO.z = args[8] / unitConv;
+		    this.emit_gcode("G28.3 Z" + (args[8] / unitConv) );
+		    MPO.z = args[8];
 		}
 		if (args[9] !== undefined) { 	//A Base Coordinate
-		    this.emit_gcode("G28.3 A" + args[9]);
-		    MPO.a = args[9] / unitConv;
+		    this.emit_gcode("G28.3 A" + (args[9] / unitConv) );
+		    MPO.a = args[9];
 		}
 		if (args[10] !== undefined) { 	//B Base Coordinate
-		    this.emit_gcode("G28.3 B" + args[10]);
-		    MPO.b = args[10] / unitConv;
+		    this.emit_gcode("G28.3 B" + (args[10] / unitConv) );
+		    MPO.b = args[10];
 		}
 		if (args[11] !== undefined) { 	//C Base Coordinate
-		    this.emit_gcode("G28.3 C" + args[11]);
-		    MPO.c = args[11] / unitConv;
+		    this.emit_gcode("G28.3 C" + (args[11] / unitConv) );
+		    MPO.c = args[11];
 		}
 		if (args[0] !== undefined) { 	//X location
 			setVA_G2.g55x = Number(((MPO.x * unitConv) - args[0]).toFixed(5));
