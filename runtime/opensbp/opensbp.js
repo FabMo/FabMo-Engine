39a9f01a0e08862a578f5996d3026a58bc76b3f2
Alex Canales <a.canales@laposte.net>
2016-01-21 16:19:50 -0500

    Correct height addition and add console.log.

--- runtime/opensbp/opensbp.js
+++ runtime/opensbp/opensbp.js
@@ -1234,16 +1234,18 @@ SBPRuntime.prototype.emit_move = function(code, pt) {
 		emit_moveContext.current_chunk.push('N' + n + ' ' + gcode);
 	};
 
-	if(this.transforms.level.apply === true  && code !== "G0") {
-		var X = (pt.X === undefined) ? emit_moveContext.cmd_posx : pt.X;
-		var Y = (pt.Y === undefined) ? emit_moveContext.cmd_posy : pt.Y;
-		var Z = (pt.Z === undefined) ? emit_moveContext.cmd_posz : pt.Z;
-		var height = leveler.findHeight(X, Y, Z);
-		if(height === false) {
-			log.error("Impossible to find the point height with the leveler.");
+		if(this.transforms.level.apply === true  && code !== "G0") {
+			var X = (pt.X === undefined) ? emit_moveContext.cmd_posx : pt.X;
+			var Y = (pt.Y === undefined) ? emit_moveContext.cmd_posy : pt.Y;
+			var Z = (pt.Z === undefined) ? emit_moveContext.cmd_posz : pt.Z;
+			var height = leveler.findHeight(X, Y, Z);
+            console.log("Z = " + Z);
+			if(height === false) {
+				log.error("Impossible to find the point height with the leveler.");
 				return;
 			}
-			pt.Z += height;
+            console.log("height = " + height);
+			pt.Z = Z + height;
 			opFunction(pt);
 			log.debug("emit_move:level");
 		}
