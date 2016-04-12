cbcb100917e0f89699ade920fde649e10be9fc2c
Jimmy Lucidarme <jlucidar@gmail.com>
2016-04-12 14:33:15 -0400

    start user kickout strategy

--- authentication.js
+++ authentication.js
@@ -3,14 +3,36 @@ var LocalStrategy   = require('passport-local').Strategy;
 var User = require('./db').User;
 
 var currentUser = null; // keep track of the current registered user
+var userTimer = 10*60*1000; // 10 minutes
+var currentUserTimer = null;
+var isCurrentUserKickeable = false;
+
+function startUserTimer(){
+  var currentUserTimer = setTimeout(userTimeout,userTimer);
+}
+
+function resetUserTimer(){
+  clearTimeout(currentUserTimer);
+  currentUserTimer = startUserTimer();
+}
+function userTimeout(){
+  isCurrentUserKickeable = true;
+}
+
 
 exports.configure = function(){
   passport.use(new LocalStrategy(
     function(username, password, done) {
-      if(currentUser && currentUser.username !== username){ // allow only one user at the time
+      if (currentUser && currentUser.username !== username && isCurrentUserKickeable && !currentUser.isAdmin){ // check if the current user is inactive, and not an admin.
+        // ask the user if he really wants to kick out the current user
+        // if so, then kick out the current user
+        // log in the new user
+
+      }else if(currentUser && currentUser.username !== username){ // checl allow only one user at the time
           console.log(currentUser);
           return done(null, false, { message: 'The user '+currentUser.username+' is already controlling the tool'});
-      }else{
+
+      }else{ // no one log in , the machine is all yours !
         User.findOne(username, function(err, user) {
           if (err) { return done(err); }
           if (!user) {
@@ -19,7 +41,10 @@ exports.configure = function(){
           if (!user.validPassword(password)) {
             return done(null, false, { message: 'Incorrect password.' });
           }
+          // success !
           currentUser = user;
+          isCurrentUserKickeable = false;
+          startUserTimer();
           return done(null, user);
         });
       }
