eeb98dfcc307a4cc5d1057d41529dc5bdc7759d0
Jimmy Lucidarme <jlucidar@gmail.com>
2016-04-22 23:39:48 +0000

    add hotplug support

--- routes/video.js
+++ routes/video.js
@@ -1,28 +1,68 @@
 var ps = require('child_process');
+var udev = require("udev");
+var webcam="/dev/video0";
 
-// create new ffmpeg processor instance using input stream
-// instead of file path (can be any ReadableStream)
-var ffmpeg = ps.spawn("gst-launch", [
+
+this.server;
+var gstreamer;
+var args=[
   "v4l2src",
-  "device=/dev/video0",
+  "device="+webcam,
   "!","image/jpeg,framerate=15/1",
   "!","queue",
   "!","fdsink"
-]);
+];
 
 
-module.exports = function(server) {
-  ffmpeg.stdout.on('data', function (data) {
+dev_list = udev.list();
+for(i in dev_list){
+  if(dev_list[i].DEVNAME===webcam){
+    //console.log("webcam hooked up");
+    gstreamer = ps.spawn("gst-launch",args);
+    configure_listener(gstreamer);
+  }
+}
+
+var monitor = udev.monitor();
+monitor.on('add', function (device) {
+  if(device.DEVNAME===webcam){
+    //console.log("webcam plugged");
+    gstreamer = ps.spawn("gst-launch",args);
+    configure_listener(gstreamer);
+  }
+});
+monitor.on('remove', function (device) {
+  if(device.DEVNAME===webcam){
+    //console.log("webcam unplugged");
+  }
+});
+monitor.on('change', function (device) {
+  if(device.DEVNAME==="/dev/video0"){
+    //console.log("webcam changed");
+  }
+});
+
+
+function configure_listener(gstreamer){
+  gstreamer.stdout.on('data', function (data) {
     //console.log(data);
     var frame = new Buffer(data).toString('base64');
     server.io.of('/video').emit('video',frame);
   });
-  ffmpeg.stderr.on('data',function(data){
-    console.log(data);
+  gstreamer.stderr.on('data',function(data){
+    //console.log(data);
   });
-  ffmpeg.on('close',function(code){
-    console.log("ffmpeg returns with code "+code);
+  gstreamer.on('close',function(code){
+    //console.log("gstreamer returns with code "+code);
   })
 
-  server.io.of('/video').on('connection',function(){console.log("client connected");})
+
+}
+
+
+module.exports = function(server) {
+  this.server=server;
+  server.io.of('/video').on('connection',function(){
+    //console.log("client connected");
+  })
 };
