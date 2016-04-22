f85ea393d75c320256acb0811373d2f7e46f00e0
Jimmy Lucidarme <jlucidar@gmail.com>
2016-04-22 19:26:48 +0000

    use gstreamer instead of ffmpeg (not working)

--- routes/video.js
+++ routes/video.js
@@ -2,21 +2,19 @@ var ps = require('child_process');
 
 // create new ffmpeg processor instance using input stream
 // instead of file path (can be any ReadableStream)
-var ffmpeg = ps.spawn("ffmpeg", [
-  "-re",
-  "-y",
-  "-i",
-  "/dev/video0",
-  "-preset",
-  "ultrafast",
-  "-f",
-  "mjpeg",
-  "pipe:1"
+var ffmpeg = ps.spawn("gst-launch", [
+  "v4l2src",
+  "device=/dev/video0",
+  "!","video/x-raw-yuv,framerate=30/1",
+  "!","queue",
+  "!","jpegenc",
+  "!","fdsink"
 ]);
 
 
 module.exports = function(server) {
   ffmpeg.stdout.on('data', function (data) {
+    //console.log(data);
     var frame = new Buffer(data).toString('base64');
     server.io.of('/video').emit('video',frame);
   });
@@ -26,4 +24,6 @@ module.exports = function(server) {
   ffmpeg.on('close',function(code){
     console.log("ffmpeg returns with code "+code);
   })
+
+  server.io.of('/video').on('connection',function(){console.log("client connected");})
 };
