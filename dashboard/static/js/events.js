922a922ac8fcb41955c1fcee5610364e1797e023
Ryan Sturmer <ryasnturmer@gmail.com>
2016-07-01 14:22:57 -0400

    Fix #470

--- dashboard/static/js/events.js
+++ dashboard/static/js/events.js
@@ -54,7 +54,7 @@ define(function(require) {
     }
     $('.DRO-button').on('click', function(evt) {
         setRightMenuBehavior();
-         var el = $(this),  
+         var el = $(this),
         newone = el.clone(true);
         el.before(newone);
         $("." + el.attr("class") + ":last").remove();
@@ -111,12 +111,12 @@ define(function(require) {
             $(".main-section").width() - ($(".main-section").width() % 132)
         )
         if ($('.axi').is(':focus')){
-            
+
         } else {
             rightMenuLoad();
         }
-      
-    
+
+
     };
 
 
@@ -126,6 +126,7 @@ define(function(require) {
     }, 1000);
 
     var colapseMenu = function() {
+
         //L & R = width of left & right menus
         var l = 0;
         var r = 0;
@@ -147,10 +148,12 @@ define(function(require) {
                 $(".main-section").width() - ($(".main-section").width() % 132)
             );
             $('.collapseLeft').hide();
-            l = parseInt($("#left-menu").css("width")) + 1; //Save left menu size		
+            l = parseInt($("#left-menu").css("width")) + 1; //Save left menu size
         }
-        //Handle collapse of left 
-        $('.collapseLeft').click(function() {
+        //Handle collapse of left
+        $('.collapseLeft').click(function(evt) {
+          console.log("collapse")
+            evt.preventDefault();
             $('.collapseLeft').hide();
             $('#left-menu').addClass("colapsed");
             $('#widget-links-general').addClass("colapsed");
@@ -230,7 +233,8 @@ define(function(require) {
         });
 
         //Idem if we colapse or un-colapse the right menu
-        $("#icon_colapse").click(function() {
+        $("#icon_colapse").click(function(evt) {
+          evt.preventDefault();
             colapseMenu();
         });
 
