5baed35cc85499d0ad9c2da61056d0a95ed5add9
Ryan Sturmer <ryasnturmer@gmail.com>
2016-09-15 10:46:25 -0400

    Fix to g2-less startup

--- engine.js
+++ engine.js
@@ -238,7 +238,11 @@ Engine.prototype.start = function(callback) {
         }.bind(this),
 
         function set_units(callback) {
-            this.machine.driver.setUnits(config.machine.get('units'), callback);
+            if(this.machine.isConnected()) {
+                this.machine.driver.setUnits(config.machine.get('units'), callback);
+            } else {
+                callback(null);
+            }
         }.bind(this),
 
         // Configure G2 by loading all its json settings and static configuration parameters
