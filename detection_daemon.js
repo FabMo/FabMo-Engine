628491350745439374b8b855e4e44961738e6b5c
Ryan Sturmer <ryansturmer@feringtech.com>
2016-09-15 13:04:51 +0000

    Re-enable bonjour

--- detection_daemon.js
+++ detection_daemon.js
@@ -17,12 +17,14 @@ var default_port = 24862; // = 7777 without conversion
 // Kick off the "detection daemon" which is the process that listens for incoming scans by the FabMo linker
 // The detection daemon is what we as a FabMo device run so that we can be discovered by the FabMo linker/dashboard
 var start = function(port) {
-//	bonjour.unpublishAll();
-//	bonjour.publish({ name: os.hostname()+" - FabMo Tool Minder daemon", host:os.hostname()+'.local', type: 'fabmo',protocol:'tcp', port: config.engine.get('server_port'),txt : {fabmo:JSON.stringify(getMachineInfo())}});
+	bonjour.unpublishAll();
+	bonjour.publish({ name: os.hostname()+" - FabMo Tool Minder daemon", host:os.hostname()+'.local', type: 'fabmo',protocol:'tcp', port: config.engine.get('server_port'),txt : {fabmo:JSON.stringify(getMachineInfo())}});
+	
 	var socketOpts = {'type':'udp4','reuseAddr':true};
 	if (process.version.match(/v0\.10\.\d*/i)) {
 		socketOpts = 'udp4';
 	}
+	
 	var socket = dgram.createSocket(socketOpts);
 	var that = this;
 	port = port || default_port;
