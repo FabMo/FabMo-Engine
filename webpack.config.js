ec0512e11f5a829937c312690341b743645610f7
Brendan Collins <brendan.c.collins@gmail.com>
2016-11-28 12:57:16 -0500

    fixing loaders

--- webpack.config.js
+++ webpack.config.js
@@ -28,26 +28,9 @@ var config = {
  },
   module: {
     loaders: [
-    { test: /\.css$/, 
-      loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-    }, {
-      test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&mimetype=application/font-woff"
-    }, {
-      test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&mimetype=application/font-woff"
-    }, {
-      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&mimetype=application/octet-stream"
-    }, {
-      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "file"
-    }, {
-      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&mimetype=image/svg+xml"
-    }, { 
-      test: /\.(png|jpg)$/, 
-      loader: 'url-loader?name=img/[name].[ext]&limit=100000' },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},   
+      { test: /\.(ttf|eot|svg|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?limit=100000' },
+      { test: /\.(png|jpg)$/, loader: 'url-loader?name=img/[name].[ext]&limit=100000' },
     ]
   },
   plugins: [
