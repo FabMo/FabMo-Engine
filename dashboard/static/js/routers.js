978506adbcb81a029b5e282fb94fc8f37d6a1805
Brendan Collins <brendan.c.collins@gmail.com>
2017-10-05 14:23:29 -0400

    router roll back

--- dashboard/static/js/routers.js
+++ dashboard/static/js/routers.js
@@ -2,88 +2,68 @@
  * routers.js defines the application routers which map URLs to application functions.
  */
 define(function(require) {
-
-	var $ = require('jquery');
-	var Backbone = require('backbone');
-	var auth = require('./auth.js');
-	var FabMoAPI = require('./libs/fabmoapi.js');
-	var engine = new FabMoAPI();
-	var defaultApp = '';
-	var hash = '';
-	engine.getConfig(function(err,data){
-		defaultApp = data.machine.default_app;
-		hash = data.engine.version.substring(0,5);
-		console.log(hash);
-		return [defaultApp, hash];
-	});
-	console.log(hash);
-	var Router = Backbone.Router.extend({
-		routes: {
-			"app/:id"     		: "redirect_app",
-			":version/app/:id"     		: "_launchApp",
-			":version"					: "show_menu",
-			""					: "redirect",
-			"authentication(?message=:message)"	: "show_auth",
-			'*notFound': 'redirect'
-
-		},
-		launchApp: function(id, args, callback) {
-			callback = callback || function() {};
-			
-			this.context.launchApp(id, args || {}, function(err, data) {
-				
-				if(err) { return callback(err); }
-				this.navigate('/'+hash+'/app/'+ id);
-			}.bind(this));
-		},
-		_launchApp: function(version, id) {
-			if (version === hash) {
+	
+		var $ = require('jquery');
+		var Backbone = require('backbone');
+		var auth = require('./auth.js');
+		var FabMoAPI = require('./libs/fabmoapi.js');
+		var engine = new FabMoAPI();
+		var defaultApp = '';
+		engine.getConfig(function(err,data){
+			return defaultApp = data.machine.default_app;
+		});
+		var Router = Backbone.Router.extend({
+			routes: {
+				"app/:id"     		: "_launchApp",
+				"menu"        		: "show_menu",
+				""					: "show_menu",
+				"authentication(?message=:message)"	: "show_auth"
+	
+			},
+			launchApp: function(id, args, callback) {
+				callback = callback || function() {};
+				this.context.launchApp(id, args || {}, function(err, data) {
+					
+					if(err) { return callback(err); }
+					this.navigate('app/' + id);
+				}.bind(this));
+			},
+			_launchApp: function(id) {
 				this.launchApp(id);
-			}else {
-				this.navigate('/'+hash+'/app/'+ id, {trigger: true});
-			}
-			
-		},
-		show_menu: function(version) {
-			if (version === hash) {
+			},
+			show_menu: function() {
 				this.launchApp(defaultApp);
+			},
+			show_auth: function (message){
+				console.log(message);
+				if (message){
+					console.log(message);
+					message.toString();
+					var par = message.replace(/-/g, ' ');
+					console.log(par);
+					$('#add_err').html('<div class="error"> <i class="fa fa-exclamation-circle" aria-hidden="true"></i><span> '+par+'</span></div>');
+					$('#add_err').removeAttr('style');
+	
+				}
+				
+				$.ajax({url: "authentication/logout", success: function(result){
+					console.log(result);
+				   }});
+				$('#mainContent').show();
+				auth();
+			},
+			loadView : function(view) {
+			this.view && this.view.remove();
+			this.view = view;
+			},
+			setContext: function(context) {
+				this.context = context;
 			}
-			else {
-				this.navigate(hash, {trigger: true});
-			}
-		},
-		redirect: function(){
-			this.navigate('/'+hash, {trigger: true});
-		},
-		redirect_app: function(id){
-			this.navigate('/'+hash+'/app/'+ id, {trigger: true});
-		},
-		show_auth: function (message){
-			if (message){
-				message.toString();
-				var par = message.replace(/-/g, ' ');
-				$('#add_err').html('<div class="error"> <i class="fa fa-exclamation-circle" aria-hidden="true"></i><span> '+par+'</span></div>');
-				$('#add_err').removeAttr('style');
-
-			}
-			
-			$.ajax({url: "authentication/logout", success: function(result){
-   			}});
-			$('#mainContent').show();
-			auth();
-		},
-		loadView : function(view) {
-		this.view && this.view.remove();
-		this.view = view;
-		},
-		setContext: function(context) {
-			this.context = context;
-		}
-		/*
-		initialize: function(options) {
-			$('a[href^="#"]').click(function(e) { this.navigate('/'); }.bind(this));
-		}*/
+			/*
+			initialize: function(options) {
+				$('a[href^="#"]').click(function(e) { this.navigate('/'); }.bind(this));
+			}*/
+		});
+	
+		return Router
 	});
-
-	return Router
-});
