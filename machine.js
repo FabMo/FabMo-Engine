0f0274a78290dbd92940c0b815d07aece4c4aa8a
Ryan Sturmer <ryansturmer@gmail.com>
2018-06-28 16:09:02 -0400

    Exception for manual drive mode

--- machine.js
+++ machine.js
@@ -348,6 +348,9 @@ Machine.prototype.arm = function(action, timeout) {
 	this.action = action;
 	var interlockRequired = config.machine.get('interlock_required');
 	var interlockInput = 'in' + config.machine.get('interlock_input');
+	if(this.action && this.action.payload.name === 'manual') {
+		interlockRequired = false;
+	}
 	if(this.action) {
 		if(interlockRequired && this.driver.status[interlockInput]) {
 			this.setState(this, 'interlock')
