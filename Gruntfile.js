4c8cb38a60a62a13e9b42b93b3863958f8f6b30e
Ryan Sturmer <Ryan.Sturmer@shopbottools.com>
2018-11-01 14:50:20 -0400

    Update grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,19 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		'groc': {
-			javascript: ["*.js", "*.md", "*/*.js", "*/*.md"],
-			options: {
-				"out": "docs-dist/"
-			},
-		},
-		'gh-pages': {
-			options: {
-				base: 'docs-dist'
-			},
-			src: ['**']
-		},
+grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        'groc': {
+            javascript: ["*.js","*.md"],
+            options: {
+                "out": "docs-dist/",
+                "except":["Gruntfile.js","dashboard/static","node_modules", "node_modules/**","dashboard/static/**"]
+            },
+        },
+        'gh-pages': {
+            options: {
+                base: 'docs-dist'
+            },
+            src: ['**']
+        },
         'open' : {
             'file' : {
                 path : 'docs-dist/index.html'
@@ -39,17 +40,15 @@ module.exports = function(grunt) {
                 debug: true
             }
         }
-	});
-	grunt.loadNpmTasks('grunt-groc');
-	grunt.loadNpmTasks('grunt-gh-pages');
-	grunt.loadNpmTasks('grunt-open');
-    grunt.loadNpmTasks('grunt-mocha-test');
+    });
+    grunt.loadNpmTasks('grunt-groc');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-open');
     grunt.loadNpmTasks('grunt-available-tasks');
     grunt.loadNpmTasks('grunt-apidoc');
 
     grunt.registerTask('doc', 'Generate engine documentation in HTML format.', ['groc', 'apidoc']);
-	grunt.registerTask('doc-dist', 'Generate documentation and publish to github-pages.', ['groc', 'apidoc', 'gh-pages']);
-	grunt.registerTask('doc-view', 'Generate documentation and view locally in web browser.', ['groc', 'apidoc', 'open']);
-    grunt.registerTask('test', 'Run tests.', 'mochaTest');
+    grunt.registerTask('doc-dist', 'Generate documentation and publish to github-pages.', ['groc', 'apidoc', 'gh-pages']);
+    grunt.registerTask('doc-view', 'Generate documentation and view locally in web browser.', ['groc', 'apidoc', 'open']);
     grunt.registerTask('default', 'availabletasks');
 };
