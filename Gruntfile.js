8cdda7db66e7770209abcd1fb0b7f11333e608a1
Ryan Sturmer <ryansturmer@gmail.com>
2018-11-01 15:23:14 -0400

    Further improvements to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function(grunt) {
 grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        'clean' : {
+            'docs' : ['docs-dist']
+        },
         'groc': {
             javascript: ["*.js","*.md","**/*.js","**/*.md"],
             options: {
@@ -17,7 +20,8 @@ grunt.initConfig({
                     "dashboard/static/**",
                     "docs-dist",
                     "docs-dist/**"
-                ]
+                ],
+                "repository-url" : "https://github.com/FabMo/FabMo-Engine.git"
             },
         },
         'gh-pages': {
@@ -54,13 +58,14 @@ grunt.initConfig({
         }
     });
     grunt.loadNpmTasks('grunt-groc');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-gh-pages');
     grunt.loadNpmTasks('grunt-open');
     grunt.loadNpmTasks('grunt-available-tasks');
     grunt.loadNpmTasks('grunt-apidoc');
 
-    grunt.registerTask('doc', 'Generate engine documentation in HTML format.', ['groc', 'apidoc']);
-    grunt.registerTask('doc-dist', 'Generate documentation and publish to github-pages.', ['groc', 'apidoc', 'gh-pages']);
-    grunt.registerTask('doc-view', 'Generate documentation and view locally in web browser.', ['groc', 'apidoc', 'open']);
+    grunt.registerTask('doc', 'Generate engine documentation in HTML format.', ['clean', 'groc', 'apidoc']);
+    grunt.registerTask('doc-dist', 'Generate documentation and publish to github-pages.', ['doc', 'gh-pages']);
+    grunt.registerTask('doc-view', 'Generate documentation and view locally in web browser.', ['doc', 'open']);
     grunt.registerTask('default', 'availabletasks');
 };
