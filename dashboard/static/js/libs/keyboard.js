0b9743776ea99c56de1522c36c987324cc08a395
tedh-shopbot <ted@shopbottools.com>
2020-08-27 17:45:47 -0700

    again

--- dashboard/static/js/libs/keyboard.js
+++ dashboard/static/js/libs/keyboard.js
@@ -146,7 +146,7 @@ Keyboard.prototype.onMouseMove = function(evt) {
 
 Keyboard.prototype.onKeyDown = function(evt) {
 console.log("===>onKeyDOWN, enabled=" + this.enabled + "  going=" + this.going)  ////##
-if(e.hasClass('drive-button-fixed')) { console.log("fixed-DOWN")} else { console.log("fixed-UP")}; ////##
+if(evt.hasClass('drive-button-fixed')) { console.log("fixed-DOWN")} else { console.log("fixed-UP")}; ////##
 	if(this.going || !this.enabled) {return}
 	this.nudgeTimer = setTimeout(function() {
 		//this.nudgeTimer = null;
