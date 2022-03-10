ccfa8edb1079a49bcb60125ae2e8f3e3d414f1d8
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2022-03-10 12:16:42 -0800

    Teds adjustments to input FH are required for spindle dwell to function properly.

--- runtime/opensbp/opensbp.js
+++ runtime/opensbp/opensbp.js
@@ -755,7 +755,6 @@ SBPRuntime.prototype._run = function() {
                         if(this.pendingFeedhold) {
                             this.pendingFeedhold = false;
                             this.driver.feedHold();
-                            this.machine.status.inFeedHold = true;
                         }
                     }
                 }
