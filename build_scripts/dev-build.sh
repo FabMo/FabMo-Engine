4ea7e7639e57df904bf37533531748d6dd7d7ab7
Jesse Vincent <jesse@keyboard.io>
2022-06-09 19:28:12 -0700

    Add the "dev-build.sh" script included in the fabmo rpi image to the repo

---
+++ build_scripts/dev-build.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# Builds my current dev version ... need to have branch identified right!!!
+
+clear
+echo "==> Building dev version"
+cd /
+echo "--> ... be at root"
+echo "--> ... clearing all Fabmo dirs"
+
+sudo systemctl stop fabmo
+echo "--> ... Stopped fabmo service"
+
+sudo rm -rf /fabmo
+sudo rm -rf /opt/fabmo
+echo
+
+echo "--> Cloning current version of FabMo Engine from GitHub"
+cd /
+sudo git clone https://github.com/fabmo/fabmo-engine ./fabmo
+
+cd /fabmo
+echo
+
+echo "--> Setting Branch"
+sudo git checkout master
+git status
+echo
+
+echo "--> Ready for npm install"
+sudo npm install
+
+echo
+echo "================================================="
+echo "==> Seem to have done it ... "
+echo "STARTING FabMo!"
+echo "================================== check path! =="
+echo
+sudo npm run dev
+
