6676cf753740341073e357f77ea627e00ae15ce8
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2022-10-31 18:41:50 -0400

    A step in the right direction, but found another way to create UI discrepancy.

--- dashboard/static/js/main.js
+++ dashboard/static/js/main.js
@@ -382,7 +382,7 @@ require("../css/toastr.min.css");
                             }
           
                         });
-                    } else if (status.state === 'lock' && status.resumeFlag === false) {
+                    } else if (status.state === 'lock' && status.resumeFlag === false && status.inFeedHold === true) {
                         interlockDialog = true;
                             keypad.setEnabled(false);
                             keyboard.setEnabled(false);
