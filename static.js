6dc4ac8935d22f0aee18ddf8e9d489d982c240bb
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2022-12-20 12:55:33 -0500

    static.js prettier changes only

--- static.js
+++ static.js
@@ -1,22 +1,21 @@
 // Copyright 2012 Mark Cavage, Inc.  All rights reserved.
 
-'use strict';
+"use strict";
 
-var fs = require('fs');
-var path = require('path');
-var escapeRE = require('escape-regexp-component');
+var fs = require("fs");
+var path = require("path");
+var escapeRE = require("escape-regexp-component");
 
-var assert = require('assert-plus');
-var mime = require('mime-types');
-var errors = require('restify-errors');
-var config = require('./config');
+var assert = require("assert-plus");
+var mime = require("mime-types");
+var errors = require("restify-errors");
+var config = require("./config");
 
 ///--- Globals
 
 var MethodNotAllowedError = errors.MethodNotAllowedError;
 var NotAuthorizedError = errors.NotAuthorizedError;
 var ResourceNotFoundError = errors.ResourceNotFoundError;
-
 
 ///--- Functions
 
@@ -33,36 +32,37 @@ var ResourceNotFoundError = errors.ResourceNotFoundError;
 function serveStatic(options) {
     var opts = options || {};
 
-    if (typeof opts.appendRequestPath === 'undefined') {
+    if (typeof opts.appendRequestPath === "undefined") {
         opts.appendRequestPath = true;
     }
 
-    assert.object(opts, 'options');
-    assert.string(opts.directory, 'options.directory');
-    assert.optionalNumber(opts.maxAge, 'options.maxAge');
-    assert.optionalObject(opts.match, 'options.match');
-    assert.optionalString(opts.charSet, 'options.charSet');
-    assert.optionalString(opts.file, 'options.file');
-    assert.bool(opts.appendRequestPath, 'options.appendRequestPath');
+    assert.object(opts, "options");
+    assert.string(opts.directory, "options.directory");
+    assert.optionalNumber(opts.maxAge, "options.maxAge");
+    assert.optionalObject(opts.match, "options.match");
+    assert.optionalString(opts.charSet, "options.charSet");
+    assert.optionalString(opts.file, "options.file");
+    assert.bool(opts.appendRequestPath, "options.appendRequestPath");
 
-    var p = path.normalize(opts.directory).replace(/\\/g, '/');
-    var re = new RegExp('^' + escapeRE(p) + '/?.*');
+    var p = path.normalize(opts.directory).replace(/\\/g, "/");
+    var re = new RegExp("^" + escapeRE(p) + "/?.*");
 
     function serveFileFromStats(file, err, stats, isGzip, req, res, next) {
-
-        if (typeof req.connectionState === 'function' &&
-            (req.connectionState() === 'close' ||
-            req.connectionState() === 'aborted')) {
+        if (
+            typeof req.connectionState === "function" &&
+            (req.connectionState() === "close" ||
+                req.connectionState() === "aborted")
+        ) {
             next(false);
             return;
         }
 
         if (err) {
-            next(new ResourceNotFoundError(err, '%s', req.path()));
+            next(new ResourceNotFoundError(err, "%s", req.path()));
             console.log("resource not found: " + file);
             return;
         } else if (!stats.isFile()) {
-            next(new ResourceNotFoundError('%s does not exist', req.path()));
+            next(new ResourceNotFoundError("%s does not exist", req.path()));
             return;
         }
 
@@ -70,31 +70,31 @@ function serveStatic(options) {
             res.handledGzip();
         }
 
-        var fstream = fs.createReadStream(file + (isGzip ? '.gz' : ''));
+        var fstream = fs.createReadStream(file + (isGzip ? ".gz" : ""));
         var maxAge = opts.maxAge === undefined ? 3600 : opts.maxAge;
-        fstream.once('open', function (fd) {
-            res.cache({maxAge: maxAge});
-            res.set('Content-Length', stats.size);
-            res.set('Content-Type', mime.lookup(file));
-            res.set('Last-Modified', stats.mtime);
+        fstream.once("open", function (fd) {
+            res.cache({ maxAge: maxAge });
+            res.set("Content-Length", stats.size);
+            res.set("Content-Type", mime.lookup(file));
+            res.set("Last-Modified", stats.mtime);
 
             if (opts.charSet) {
-                var type = res.getHeader('Content-Type') +
-                    '; charset=' + opts.charSet;
-                res.setHeader('Content-Type', type);
+                var type =
+                    res.getHeader("Content-Type") + "; charset=" + opts.charSet;
+                res.setHeader("Content-Type", type);
             }
 
             if (opts.etag) {
-                res.set('ETag', opts.etag(stats, opts));
+                res.set("ETag", opts.etag(stats, opts));
             }
             res.writeHead(200);
             fstream.pipe(res);
-            fstream.once('close', function () {
+            fstream.once("close", function () {
                 next(false);
             });
         });
 
-        res.once('close', function () {
+        res.once("close", function () {
             fstream.close();
         });
     }
@@ -105,98 +105,89 @@ function serveStatic(options) {
                 // Serve an index.html page or similar
                 var filePath = path.join(file, opts.default);
                 fs.stat(filePath, function (dirErr, dirStats) {
-                    serveFileFromStats(filePath,
+                    serveFileFromStats(
+                        filePath,
                         dirErr,
                         dirStats,
                         false,
                         req,
                         res,
-                        next);
+                        next
+                    );
                 });
             } else {
-                serveFileFromStats(file,
-                    err,
-                    stats,
-                    false,
-                    req,
-                    res,
-                    next);
+                serveFileFromStats(file, err, stats, false, req, res, next);
             }
         });
     }
 
     function serve(req, res, next) {
         var file;
-        var path_arr = req.path().split('/')
+        var path_arr = req.path().split("/");
 
-        for(var i = 0; i < path_arr.length; i++){
-            if(path_arr[i] === config.engine.get('version')) {
+        for (var i = 0; i < path_arr.length; i++) {
+            if (path_arr[i] === config.engine.get("version")) {
                 path_arr.splice(i, 1);
             }
         }
 
         if (path_arr.length > 1) {
-            var newPath = path_arr.join('/');
+            var newPath = path_arr.join("/");
         } else {
-            var newPath = '/';
+            var newPath = "/";
         }
-       
+
         req.url = newPath;
 
         if (opts.file) {
             //serves a direct file
-            file = path.join(opts.directory,
-                decodeURIComponent(opts.file));
+            file = path.join(opts.directory, decodeURIComponent(opts.file));
         } else {
-
             if (opts.appendRequestPath) {
-                file = path.join(opts.directory,
-                    decodeURIComponent(req.path()));
-            }
-            else {
+                file = path.join(
+                    opts.directory,
+                    decodeURIComponent(req.path())
+                );
+            } else {
                 var dirBasename = path.basename(opts.directory);
                 var reqpathBasename = path.basename(req.path());
 
-                if (path.extname(req.path()) === '' &&
-                    dirBasename === reqpathBasename) {
-     
+                if (
+                    path.extname(req.path()) === "" &&
+                    dirBasename === reqpathBasename
+                ) {
                     file = opts.directory;
-                }
-                else {
-                    file = path.join(opts.directory,
-                        decodeURIComponent(path.basename(req.path())));
+                } else {
+                    file = path.join(
+                        opts.directory,
+                        decodeURIComponent(path.basename(req.path()))
+                    );
                 }
             }
         }
-        if (req.method !== 'GET' && req.method !== 'HEAD') {
+        if (req.method !== "GET" && req.method !== "HEAD") {
             console.log("static open 1 bad method: " + file);
             next(new MethodNotAllowedError(req.method));
             return;
         }
 
-        if (!re.test(file.replace(/\\/g, '/'))) {
+        if (!re.test(file.replace(/\\/g, "/"))) {
             console.log("static open 1 could not replace slashes: " + file);
-            next(new NotAuthorizedError('%s', req.path()));
+            next(new NotAuthorizedError("%s", req.path()));
             return;
         }
 
         if (opts.match && !opts.match.test(file)) {
             console.log("static open 1 could not authorized: " + file);
-            next(new NotAuthorizedError('%s', req.path()));
+            next(new NotAuthorizedError("%s", req.path()));
             return;
         }
 
-        if (opts.gzip && req.acceptsEncoding('gzip')) {
-            fs.stat(file + '.gz', function (err, stats) {
+        if (opts.gzip && req.acceptsEncoding("gzip")) {
+            fs.stat(file + ".gz", function (err, stats) {
                 if (!err) {
-                    res.setHeader('Content-Encoding', 'gzip');
-                    serveFileFromStats(file,
-                        err,
-                        stats,
-                        true,
-                        req,
-                        res,
-                        next);
+                    res.setHeader("Content-Encoding", "gzip");
+                    serveFileFromStats(file, err, stats, true, req, res, next);
                 } else {
                     serveNormal(file, req, res, next);
                 }
@@ -204,10 +195,9 @@ function serveStatic(options) {
         } else {
             serveNormal(file, req, res, next);
         }
-
     }
 
-    return (serve);
+    return serve;
 }
 
 module.exports = serveStatic;
