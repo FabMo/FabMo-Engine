c6ba908500b3afd845c27a16a4993c534c41c949
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2022-12-20 13:03:53 -0500

    server.js linting complete

--- server.js
+++ server.js
@@ -5,11 +5,11 @@
  *
  * This is the entry point for the fabmo engine.  It just starts the engine.
  */
-var config = require("./config");
-var log = require("./log").logger("server");
 var engine = require("./engine");
-var argv = require("minimist")(process.argv);
+// eslint-disable-next-line no-undef
+var argv = require("minimist")(process.argv); // process is undefined
 
+// eslint-disable-next-line no-unused-vars
 engine.start(function (err, data) {
     // Start the debug monitor if requested, but only after the engine is fully started
     if ("debug" in argv) {
