2c5e8ff3e1bdb7ce9a725cea8ce9b9b15ad9f5c4
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2022-12-22 11:04:40 -0500

    Gruntfile.js linting complete, only prettier changes required

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,14 @@
-module.exports = function(grunt) {
-grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        'clean' : {
-            'docs' : ['docs-dist']
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        clean: {
+            docs: ["docs-dist"],
         },
-        'groc': {
-            javascript: ["*.js","*.md","**/*.js","**/*.md"],
+        groc: {
+            javascript: ["*.js", "*.md", "**/*.js", "**/*.md"],
             options: {
-                "out": "docs-dist/",
-                "except":[
+                out: "docs-dist/",
+                except: [
                     "Gruntfile.js",
                     "dashboard/apps",
                     "dashboard/apps/**",
@@ -19,48 +19,60 @@ grunt.initConfig({
                     "dashboard/static",
                     "dashboard/static/**",
                     "docs-dist",
-                    "docs-dist/**"
+                    "docs-dist/**",
                 ],
-                "repository-url" : "https://github.com/FabMo/FabMo-Engine.git"
+                "repository-url": "https://github.com/FabMo/FabMo-Engine.git",
             },
         },
-        'gh-pages': {
+        "gh-pages": {
             options: {
-                base: 'docs-dist'
+                base: "docs-dist",
             },
-            src: ['**']
+            src: ["**"],
         },
-        'open' : {
-            'file' : {
-                path : 'docs-dist/index.html'
-            }
+        open: {
+            file: {
+                path: "docs-dist/index.html",
+            },
         },
-        'availabletasks' : {
-            'tasks' : {
+        availabletasks: {
+            tasks: {
                 options: {
-                    showTasks: ['user'],
-                    filter: 'exclude',
-                    tasks: ['availabletasks', 'default']
-                }
-            }
+                    showTasks: ["user"],
+                    filter: "exclude",
+                    tasks: ["availabletasks", "default"],
+                },
+            },
         },
-        'apidoc' : {
+        apidoc: {
             myapp: {
                 src: "routes",
                 dest: "docs-dist/api",
-                debug: true
-            }
-        }
+                debug: true,
+            },
+        },
     });
-    grunt.loadNpmTasks('grunt-groc');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-gh-pages');
-    grunt.loadNpmTasks('grunt-open');
-    grunt.loadNpmTasks('grunt-available-tasks');
-    grunt.loadNpmTasks('grunt-apidoc');
+    grunt.loadNpmTasks("grunt-groc");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-gh-pages");
+    grunt.loadNpmTasks("grunt-open");
+    grunt.loadNpmTasks("grunt-available-tasks");
+    grunt.loadNpmTasks("grunt-apidoc");
 
-    grunt.registerTask('doc', 'Generate engine documentation in HTML format.', ['clean', 'groc', 'apidoc']);
-    grunt.registerTask('doc-dist', 'Generate documentation and publish to github-pages.', ['doc', 'gh-pages']);
-    grunt.registerTask('doc-view', 'Generate documentation and view locally in web browser.', ['doc', 'open']);
-    grunt.registerTask('default', 'availabletasks');
+    grunt.registerTask("doc", "Generate engine documentation in HTML format.", [
+        "clean",
+        "groc",
+        "apidoc",
+    ]);
+    grunt.registerTask(
+        "doc-dist",
+        "Generate documentation and publish to github-pages.",
+        ["doc", "gh-pages"]
+    );
+    grunt.registerTask(
+        "doc-view",
+        "Generate documentation and view locally in web browser.",
+        ["doc", "open"]
+    );
+    grunt.registerTask("default", "availabletasks");
 };
