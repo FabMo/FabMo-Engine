0dd33a9f1330e7dc86ad11505922913c4f891b08
kyle-kingsbury <kyle.kingsbury@shopbottools.com>
2022-12-23 12:31:37 -0500

    runtime/opensbp/commands/custom.js linting complete

--- runtime/opensbp/commands/custom.js
+++ runtime/opensbp/commands/custom.js
@@ -1,8 +1,3 @@
-var log = require("../../../log").logger("sbp");
-var g2 = require("../../../g2");
-var sb3_commands = require("../sb3_commands");
-var config = require("../../../config");
-
 /* CUSTOM CUTS */
 
 // Set to table base coordinates
