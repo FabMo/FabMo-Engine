bb25e8e2531875ecd3560c233946aa6929e594ce
tedh-shopbot <ted@shopbottools.com>
2025-08-10 11:30:25 -0700

    sb4 ver 4.0.85

--- dashboard/apps/sb4.fma/js/init_setActions.js
+++ dashboard/apps/sb4.fma/js/init_setActions.js
@@ -499,13 +499,13 @@ $(document).ready(function () {
 
     // Tool Tips
     $("#cmd-ref").click(function(evt) {
-        getUsrResource('docs/ComRef.pdf', 'docs/ComRef.pdf');      // Open the local ComRef.pdf in the dashboard docs folder for the moment 
+        getUsrResource('https://shopbottools.com/wp-content/uploads/2025/07/ComRef.pdf', 'docs/ComRef.pdf');   // Testing the online ShopBot Host 
     });
     $("#cmd-ref2").click(function(evt) {
-        getUsrResource('https://shopbottools.com/wp-content/uploads/2025/07/ComRef.pdf', 'docs/ComRef.pdf');      // Testing the online ComRef.pdf
+        getUsrResource('https://shopbottools.com/wp-content/uploads/2025/07/ComRef.pdf', 'docs/ComRef.pdf');   // Testing the online ShopBot Host
     });
     $("#ck-list").click(function(evt) {
-        getUsrResource('docs/ShopBot_checklist.pdf', 'docs/ShopBot_checklist.pdf');      // Open the local ComRef.pdf in the dashboard docs folder for the moment 
+        getUsrResource('docs/ShopBot_checklist.pdf', 'docs/ShopBot_checklist.pdf');      // Open only the local version of checklist in fabmo
     });
 
 
@@ -621,30 +621,32 @@ $(document).ready(function () {
         }
     }
 
-    $("#vid-button").click(function () {      // Toggle video
+    $("#vid-button").click(function () {      // Toggle video as long as we have something
         // Check for toggle state and change if video present
-        if ($("#vid-button").hasClass("vid-button-on")) {
-            $("#vid-button").removeClass("vid-button-on");
-            $("#vid-button").addClass("vid-button-off");
-            if (localStorage.getItem("fabmo_sb4_has_video") === "true") {
-                $("#video").css("visibility", "visible");
-                $("#file_txt_area").css("background", "#327c7e");
-                localStorage.setItem("fabmo_sb4_video_button", 1); // 1 video present, but not playing; 
-            } else {
-                $("#video").css("visibility", "hidden");
-                $("#file_txt_area").css("background", "#327c7e");
-                localStorage.setItem("fabmo_sb4_video_button", 0); // 0 video disabled
+        if ((localStorage.getItem("fabmo_sb4_video-button") !== "0") && (localStorage.getItem("fabmo_sb4_has_video") === "true")) {
+            if ($("#vid-button").hasClass("vid-button-on")) {
+                $("#vid-button").removeClass("vid-button-on");
+                $("#vid-button").addClass("vid-button-off");
+                    if (localStorage.getItem("fabmo_sb4_has_video") === "true") {
+                        $("#video").css("visibility", "visible");
+                        $("#file_txt_area").css("background", "#327c7e");
+                        localStorage.setItem("fabmo_sb4_video_button", 1); // 1 video present, but not playing; 
+                    } else {
+                        $("#video").css("visibility", "hidden");
+                        $("#file_txt_area").css("background", "#327c7e");
+                        localStorage.setItem("fabmo_sb4_video_button", 0); // 0 video disabled
+                    }
+                } else {
+                $("#vid-button").removeClass("vid-button-off");
+                $("#vid-button").addClass("vid-button-on");
+                    $("#video").css("visibility", "visible");
+                    $("#file_txt_area").css("background", "transparent");
+                    localStorage.setItem("fabmo_sb4_video_button", 2); // 2 video present and playing
+                    localStorage.setItem("fabmo_sb4_has_video", "true");
+                }
             }
-        } else {
-            $("#vid-button").removeClass("vid-button-off");
-            $("#vid-button").addClass("vid-button-on");
-            $("#video").css("visibility", "visible");
-            $("#file_txt_area").css("background", "transparent");
-            localStorage.setItem("fabmo_sb4_video_button", 2); // 2 video present and playing
-            localStorage.setItem("fabmo_sb4_has_video", "true");
-        }
-        saveUIConfig();
-    });
+    saveUIConfig();
+});
 
     //** Try to restore CMD focus when there is a shift back to app
     $(document).click(function (e) {
